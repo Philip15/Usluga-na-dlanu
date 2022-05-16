2fee9b8bef14523b868b0e6e1fd9b0b8907b6f1d
janap15 <jana.pasajlic@gmail.com>
2022-05-16 18:41:48 +0200

    Changed db name

--- docs/Modelovanje Baze/usluga_na_dlanu.sql
+++ docs/Modelovanje Baze/usluga_na_dlanu.sql
@@ -5,22 +5,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema usluga_na_dlanu
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `mydb` ;
+DROP SCHEMA IF EXISTS `usluga_na_dlanu` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema usluga_na_dlanu
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `usluga_na_dlanu` DEFAULT CHARACTER SET utf8 ;
+USE `usluga_na_dlanu` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`kategorije`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`kategorije` ;
+DROP TABLE IF EXISTS `usluga_na_dlanu`.`kategorije` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`kategorije` (
+CREATE TABLE IF NOT EXISTS `usluga_na_dlanu`.`kategorije` (
   `idKategorije` INT NOT NULL AUTO_INCREMENT,
   `naziv` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idKategorije`),
@@ -29,11 +29,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`korisnici`
+-- Table `usluga_na_dlanu`.`korisnici`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`korisnici` ;
+DROP TABLE IF EXISTS `usluga_na_dlanu`.`korisnici` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`korisnici` (
+CREATE TABLE IF NOT EXISTS `usluga_na_dlanu`.`korisnici` (
   `idKorisnika` INT NOT NULL AUTO_INCREMENT,
   `korisnickoIme` VARCHAR(45) NOT NULL,
   `lozinka` VARCHAR(45) NOT NULL,
@@ -55,18 +55,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`korisnici` (
   INDEX `idKategorije_idx` (`idKategorije` ASC) VISIBLE,
   CONSTRAINT `idKategorije`
     FOREIGN KEY (`idKategorije`)
-    REFERENCES `mydb`.`kategorije` (`idKategorije`)
+    REFERENCES `usluga_na_dlanu`.`kategorije` (`idKategorije`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`zahtevi`
+-- Table `usluga_na_dlanu`.`zahtevi`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`zahtevi` ;
+DROP TABLE IF EXISTS `usluga_na_dlanu`.`zahtevi` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`zahtevi` (
+CREATE TABLE IF NOT EXISTS `usluga_na_dlanu`.`zahtevi` (
   `idZahteva` INT NOT NULL AUTO_INCREMENT,
   `idKorisnika` INT NOT NULL,
   `idPruzaoca` INT NOT NULL,
@@ -82,23 +82,23 @@ CREATE TABLE IF NOT EXISTS `mydb`.`zahtevi` (
   INDEX `idKorisnika_idx` (`idKorisnika` ASC) VISIBLE,
   CONSTRAINT `idKorisnika`
     FOREIGN KEY (`idKorisnika`)
-    REFERENCES `mydb`.`korisnici` (`idKorisnika`)
+    REFERENCES `usluga_na_dlanu`.`korisnici` (`idKorisnika`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `idPruzaoca2`
     FOREIGN KEY (`idPruzaoca`)
-    REFERENCES `mydb`.`korisnici` (`idKorisnika`)
+    REFERENCES `usluga_na_dlanu`.`korisnici` (`idKorisnika`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`termini`
+-- Table `usluga_na_dlanu`.`termini`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`termini` ;
+DROP TABLE IF EXISTS `usluga_na_dlanu`.`termini` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`termini` (
+CREATE TABLE IF NOT EXISTS `usluga_na_dlanu`.`termini` (
   `idTermina` INT NOT NULL AUTO_INCREMENT,
   `idPruzaoca` INT NOT NULL,
   `datumVremePocetka` DATETIME NOT NULL,
@@ -109,12 +109,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`termini` (
   INDEX `idZahteva_idx` (`idZahteva` ASC) VISIBLE,
   CONSTRAINT `idPruzaoca`
     FOREIGN KEY (`idPruzaoca`)
-    REFERENCES `mydb`.`korisnici` (`idKorisnika`)
+    REFERENCES `usluga_na_dlanu`.`korisnici` (`idKorisnika`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `idZahteva`
     FOREIGN KEY (`idZahteva`)
-    REFERENCES `mydb`.`zahtevi` (`idZahteva`)
+    REFERENCES `usluga_na_dlanu`.`zahtevi` (`idZahteva`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
